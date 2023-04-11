02f21d616a14dcde78749f0b4c96f746a2878d6f
Rishit Shivesh <rishitshivesh@gmail.com>
2023-04-11 09:26:13 +0530

    Initialize and Finish

---
+++ src/components/pages/NotFound/index.js
@@ -0,0 +1,11 @@
+import "./index.css";
+
+const NotFound = () => {
+  return (
+    <div className="container page-not-found">
+      <h2>Uh on! This page isn't on our menu.</h2>
+    </div>
+  );
+};
+
+export default NotFound;
